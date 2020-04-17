290f2f6a7ded29b017a2540c3106e3b4077d4b27
AungPhoneKhant <akha0030@student.monash.edu>
2020-04-17 22:50:02 +1000

    Updates

---
+++ precache-manifest.1c1647bbc570b4c2fced2c4a6f801a18.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bccf9e3f644c20e498355a3922d35d22",
+    "url": "/portfolio-design/index.html"
+  },
+  {
+    "revision": "773ca9047b8154f30d16",
+    "url": "/portfolio-design/static/css/main.a7ea41fa.chunk.css"
+  },
+  {
+    "revision": "cbb52392703e53847290",
+    "url": "/portfolio-design/static/js/2.1974c4ad.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio-design/static/js/2.1974c4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "773ca9047b8154f30d16",
+    "url": "/portfolio-design/static/js/main.dd425c7b.chunk.js"
+  },
+  {
+    "revision": "eca6900feb03379a2f25",
+    "url": "/portfolio-design/static/js/runtime-main.7d890a07.js"
+  },
+  {
+    "revision": "5a42df19ad9d8bd2c3a196733b57b4ac",
+    "url": "/portfolio-design/static/media/arduino_logo.5a42df19.png"
+  },
+  {
+    "revision": "e79e5dbde23109879fa90860aa73499c",
+    "url": "/portfolio-design/static/media/c_logo.e79e5dbd.png"
+  },
+  {
+    "revision": "1399739030aea41605dfe91130605fcc",
+    "url": "/portfolio-design/static/media/heartrate_sensor.13997390.jpg"
+  },
+  {
+    "revision": "cf28632239db6e02bc09bcca6bf5360f",
+    "url": "/portfolio-design/static/media/matlab_logo.cf286322.png"
+  }
+]);
